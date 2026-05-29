0ce7ff589f37d3e07258413c821a5005fd3afcb3
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-05-29 18:16:42 -0400

    agrega sistema celulares_basicos

---
+++ 02_poo/clases_objetos/celulares_basicos/Celular.java
@@ -0,0 +1,36 @@
+package celulares_basicos;
+
+/**
+ * Clase que representa un Celular
+ * que contiene como atributos marca, especie y edad
+ * como tambien unos metodos que ejecutan una accion de llamar() y cargarBateria()
+ */
+
+public class Celular {
+    String marca,almacenamiento;
+    int bateria;
+
+
+    public Celular(String marca, String almacenamiento, int bateria) {
+        this.marca = marca;
+        this.almacenamiento = almacenamiento;
+        this.bateria = bateria;
+    }
+
+    public void llamar(){
+        System.out.println("llamando desde " + this.marca);
+    }
+
+    public void cargarBateria(){
+        System.out.println("cargando bateria de " +this.marca + " %" + this.bateria);
+    }
+
+    @Override
+    public String toString() {
+        return "Celular{" +
+                "marca: " + marca + '\'' +
+                ", almacenamiento: " + almacenamiento + '\'' +
+                ", bateria: " + bateria +
+                '}';
+    }
+}
