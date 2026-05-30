5bd9d91613d227bef74944fde8376a48ad2c51ba
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-05-30 16:41:45 -0400

    agrega registroEstudiante y registroEmpleado

---
+++ 01-fundamentos/01-variables/01-registro-estudiante/src/registroEstudiante/Main.java
@@ -0,0 +1,19 @@
+package registroEstudiante;
+
+public class Main {
+    public static void main(String[] args){
+
+        String nombre = "Alex";
+        int edad = 27;
+        double promedio = 6.6;
+        char curso = 'B';
+        boolean beca = true;
+
+        System.out.println("\n===INFORMACIÓN ESTUDIANTE===");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Curso: " + curso);
+        System.out.println("Beca: " + beca);
+    }
+}
---
+++ 01-fundamentos/01-variables/02-registro-empleado/src/registroEmpleado/Main.java
@@ -0,0 +1,18 @@
+package registroEmpleado;
+
+public class Main {
+    public static void main(String[] args){
+        String nombre = "Marco";
+        int edad = 28;
+        long sueldo = 5000000;
+        char categoria = 'C';
+        boolean contratoIndefinido = false;
+
+        System.out.println("\n====INFORMACIÓN EMPLEADO====");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Sueldo: " + sueldo);
+        System.out.println("Categoria:" + categoria);
+        System.out.println("Contrato indefinido: " + contratoIndefinido);
+    }
+}
