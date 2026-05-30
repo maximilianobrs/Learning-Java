617858ba39dccbaa56313afe5778c28049a20f79
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-05-30 17:25:13 -0400

    agrega 06-datos-cuenta-bancaria

---
+++ 01-fundamentos/01-variables/06-datos-cuenta-bancaria/src/datosCuentaBancaria/Main.java
@@ -0,0 +1,20 @@
+package datosCuentaBancaria;
+/**
+ * Programa que muestra la información de una cuenta bancaria.
+ */
+public class Main {
+    public static void main(String[] args){
+        String titular = "Carlos Mendoza";
+        String numeroCuenta = "001122334455";
+        long saldo = 1500000;
+        char tipoCuenta = 'C';
+        boolean cuentaActiva = true;
+
+        System.out.println("\n=== ESTADO DE CUENTA BANCARIA ===");
+        System.out.println("Titular de la Cuenta: " + titular);
+        System.out.println("Número de Cuenta: " + numeroCuenta);
+        System.out.println("Saldo Disponible: " + saldo);
+        System.out.println("Tipo de Cuenta: " + tipoCuenta);
+        System.out.println("Cuenta Activa: " + cuentaActiva);
+    }
+}
