f17517e0d6bd3a56284bc194485e33647d7f76e1
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-05-31 15:02:00 -0400

    agrega 03-calculo-edad-futura

---
+++ 01-fundamentos/02-operadores/03-calculo-edad-futura/src/calculoEdadFutura/Main.java
@@ -0,0 +1,19 @@
+package calculoEdadFutura;
+
+/**
+ * Clase principal para el cálculo proyectado de la edad de un usuario.
+ * Demuestra la aplicación de operadores aritméticos simples para la actualización de datos.
+ */
+
+public class Main {
+    public static void main(String[] args){
+        int edadActual = 20;
+        int anioFuturo = 11;
+        int edadFutura = edadActual + anioFuturo;
+
+        System.out.println("\n=== Cálculo de edad futura ===");
+        System.out.println("Edad actual: " + edadActual);
+        System.out.println("Años a futuro: " + anioFuturo);
+        System.out.println("Edad futura: " + edadFutura);
+    }
+}
