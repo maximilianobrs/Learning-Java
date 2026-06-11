460cdcc2f6a83f10984cd8244ea3d1254b5f7c93
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-06-11 19:01:13 -0400

    agrega sistema de ciclo contador del 1 al 10

---
+++ 01-fundamentos/04-ciclos/01-contador-del-1-al-10/src/contador/Main.java
@@ -0,0 +1,14 @@
+package contador;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //contador del 1 al 10
+        System.out.println("\n=== RESULTADO ===\n");
+
+        for (int n = 1; n <= 10 ; n++){
+            System.out.println(n);
+        }
+
+    }
+}
