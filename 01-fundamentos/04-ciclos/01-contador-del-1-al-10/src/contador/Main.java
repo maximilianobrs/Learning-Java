f43e37b28eb03bf21c8832062aad1f127502f815
Maximiliano Briones <Maximilianonox@hotmail.com>
2026-06-11 19:06:15 -0400

    agrega sistema de cuenta regresiva

--- 01-fundamentos/04-ciclos/01-contador-del-1-al-10/src/contador/Main.java
+++ 01-fundamentos/04-ciclos/01-contador-del-1-al-10/src/contador/Main.java
@@ -3,7 +3,7 @@ package contador;
 public class Main {
     public static void main(String[] args) {
 
-        //contador del 1 al 10
+        //contador del 1 al 10g
         System.out.println("\n=== RESULTADO ===\n");
 
         for (int n = 1; n <= 10 ; n++){
---
+++ 01-fundamentos/04-ciclos/02-contador-regresivo/src/contadorRegresivo/Main.java
@@ -0,0 +1,14 @@
+package contadorRegresivo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\n=== CUENTA REGRESIVA ===\n");
+
+        //cuenta degresiva del 10 al 1
+        for (int n = 10;n>=1;n--){
+            System.out.println(n);
+        }
+
+    }
+}
